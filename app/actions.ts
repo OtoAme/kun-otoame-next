757831c177b7d4e36c18abf82bbafcbe3bd5e193
otoamegame <otoamegame@gmail.com>
2025-12-05 11:22:19 +0800

    perf(home): force static SFW content to fix server-side caching

--- app/actions.ts
+++ app/actions.ts
@@ -4,11 +4,9 @@ import { setKUNGalgameTask } from '~/server/cron'
 
 setKUNGalgameTask()
 
-import { getNSFWHeader } from '~/utils/actions/getNSFWHeader'
 import { getHomeData } from '~/app/api/home/route'
 
 export const kunGetActions = async () => {
-  const nsfwEnable = await getNSFWHeader()
-  const response = await getHomeData(nsfwEnable)
+  const response = await getHomeData({ content_limit: 'sfw' })
   return response
 }
