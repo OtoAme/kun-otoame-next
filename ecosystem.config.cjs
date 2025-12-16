d51c6d5d1887d4b7efaf55d123f80dff4b514296
otoamegame <otoamegame@gmail.com>
2025-12-16 22:39:10 +0800

    fix(deploy): update PM2 cwd to standalone directory for correct server startup

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,12 +5,12 @@ module.exports = {
     {
       name: 'kun-touchgal-next',
       port: 3000,
-      cwd: path.join(__dirname),
+      cwd: path.join(__dirname, '.next', 'standalone'),
       instances: 3,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      script: './.next/standalone/server.js',
+      script: 'server.js',
       // https://nextjs.org/docs/pages/api-reference/config/next-config-js/output
       env: {
         NODE_ENV: 'production',
