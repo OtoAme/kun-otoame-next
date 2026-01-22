56110febdf259f6eb3abe16bbb76de4746b535b0
otoamegame <otoamegame@gmail.com>
2026-01-22 16:59:06 +0800

    feat(analytics): add Google Analytics tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,19 @@ export default function RootLayout({
           src="https://uma.209911.xyz/script.js"
           data-website-id="e01c3e18-52d6-4057-b1d4-1fa00c45b9ac"
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LZ22SY3YL0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-LZ22SY3YL0');
+          `}
+        </Script>
         <Providers>
           <div className="relative flex flex-col items-center justify-center min-h-screen bg-radial">
             <KunTopBar />
